3994913ff88727b728684922094703c439ba782e
Kelsey Fecho <kfecho30@gmail.com>
2018-07-20 21:21:23 +0000

    automatically backed up by learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,2 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+
+export default class Invitation extends React.Component {
+  render(){
+    return 
+    (
+      
+    )
+  }
+}
+
